0723f95f0562e91e3572b71806c33a0717642cc6
이혜진 <hy2jinl22@gmail.com>
2021-09-18 22:13:42 +0900

    BOJ 1068 트리 (dfs)

--- study_gold/dfs-bfs/1068.py
+++ study_gold/dfs-bfs/1068.py
@@ -5,32 +5,40 @@ sys.stdin = open('1068.txt')
 
 def dfs(v):
     global cnt
+    if not adj[v]:
+        cnt += 1
+        return
     for w in adj[v]:
-        if w != d and not visited[w]:
-            visited[w] = 1
-            dfs(w)
-            if not adj[w]:
-                cnt += 1
+        dfs(w)
+
+
+def remove(v):
+    while adj[v]:
+        w = adj[v].pop()
+        remove(w)
 
 
 N = int(input())
 pa = list(map(int, input().split()))
 d = int(input())
-# print('pa :', pa)
-
 adj = [[] for _ in range(N)]
-for i in range(N):
-    if pa[i] == -1:
+for i, p in enumerate(pa):
+    if p == -1:
         s = i
     else:
-        adj[pa[i]].append(i)
+        adj[p].append(i)
+    if i == d:
+        del_p = p
+
 if s == d:
     print(0)
     exit()
-# print('adj :', adj)
+
+# print('before_adj :', adj)
+remove(d)
+adj[del_p].remove(d)
+# print('after_adj :', adj)
+
 cnt = 0
-visited = [0] * N
-visited[s] = 1
-# print('start point :', s)
 dfs(s)
-print(cnt) if cnt else print(1)
+print(cnt)
