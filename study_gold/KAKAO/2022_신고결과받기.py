bfa5db6d27e43cbce910e2552795591e4e39445b
이혜진 <hy2jinl22@gmail.com>
2022-02-09 19:17:16 +0900

    2022 kakao blind recruitment 신고 결과 받기

--- study_gold/KAKAO/2022_신고결과받기.py
+++ study_gold/KAKAO/2022_신고결과받기.py
@@ -1,27 +1,27 @@
+# 2022 kakao blind recruitment 신고 결과 받기
+
 def solution(id_list, report, k):
     n = int(k)
     answer = []
 
-    # 누가 누구를 신고했는지
-    dic = {id: set() for id in id_list}
+    dic = {id: set() for id in id_list}         # 누가 누구를 신고했는지
     for rep in report:
         k, v = rep.split()
         dic[k].add(v)
+    # print(dic)
 
-    # 누가 몇번 신고당했는지
-    x_dic = {id: 0 for id in id_list}
+    x_dic = {id: 0 for id in id_list}           # 누가 몇번 신고당했는지
     for k in dic:
         for v in dic[k]:
             x_dic[v] += 1
+    # print(x_dic)
 
-    # 신고 횟수 이상인것만 남기기
-    for k in dic:
+    for k in dic:                               # 신고 횟수 부족한 경우 제외
         dic[k] = list(dic[k])
         for i in range(len(dic[k])-1, -1, -1):
             if x_dic[dic[k][i]] < n:
                 dic[k].pop()
-        answer.append(len(dic[k]))
-
+        answer.append(len(dic[k]))              # 몇번 연락받는가?
     return answer
 
 a = solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"], 2)
