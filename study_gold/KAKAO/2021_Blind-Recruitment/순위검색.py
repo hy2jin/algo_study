ac4c07dfedd9e9b8ee256711decd8ba41e8fbd67
이혜진 <hy2jinl22@gmail.com>
2022-03-10 02:48:08 +0900

    ing 순위검색

---
+++ study_gold/KAKAO/2021_Blind-Recruitment/순위검색.py
@@ -0,0 +1,15 @@
+# 2021 Blind Recruitment 순위 검색
+'''
+조합과 이진탐색을 이용해야 효율성 테스트를 통과할 수 있다
+'''
+
+info = ["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"]
+query = ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]
+result = [1,1,1,1,2,4]
+
+def solution(info, query):
+    answer = []
+    return answer
+
+print(solution(info, query))
+
